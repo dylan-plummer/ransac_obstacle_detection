3c99a91e1d1a9c50d0224d735784966fb28ede2a
dylan <dylanmplummer@gmail.com>
2018-10-27 12:53:55 -0400

    Add Quaternion message to properly send x, y, z, and diameter.  Add custom listener.

--- ros_publish.py
+++ ros_publish.py
@@ -1,22 +1,23 @@
 import rospy
-from std_msgs.msg import String
+from geometry_msgs.msg import Quaternion
 #import Obstacle
 
 topic = 'obstacles'
-node_name = 'obstacle detection'
+node_name = 'master'
 
 def send_obstacle_data(obs):
     global topic
     global node_name
     try:
-        pub = rospy.Publisher(topic, String)
-	#msg = Obstacle()
-	#msg.x = obs.x
-	#msg.y = obs.y
-	#msg.z = obs.z
-	#msg.diameter = obs.diameter
+        pub = rospy.Publisher(topic, Quaternion)
+	msg = Quaternion()
+	msg.x = obs.x
+	msg.y = obs.y
+	msg.z = obs.z
+	msg.w = obs.diameter
         rospy.init_node(node_name, anonymous=True)
-        rospy.loginfo(str(obs))
-        pub.publish(str(obs))
-    except rospy.ROSInterruptException:
+        rospy.loginfo(msg)
+        pub.publish(msg)
+    except rospy.ROSInterruptException as e:
+	print(e.getMessage())
         pass
